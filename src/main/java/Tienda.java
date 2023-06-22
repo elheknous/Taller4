db305c3bb219343794d2fb14948cb15961892463
jose matus <josematustoro6@gmail.com>
2023-06-22 16:31:32 -0400

    Tests, funciones, y UML mejorado

--- src/main/java/Tienda.java
+++ src/main/java/Tienda.java
@@ -16,6 +16,10 @@ public class Tienda {
         this.clientes = clientes;
     }
 
+    public Tienda(ArrayList<Tablets> tablets) {
+        this.tablets = tablets;
+    }
+
     public Tienda() {
     }
 
@@ -60,7 +64,6 @@ public class Tienda {
     }
 
     public void agregarCliente(){
-
         Scanner sc = new Scanner(System.in);
         System.out.println("Nombre: ");
         String nombre = sc.next();
@@ -82,6 +85,126 @@ public class Tienda {
     public Cliente crearCliente(String nombre, String apellido, String correo, String numero, String estadoCivil,String ciudad) {
         Cliente aux = new Cliente(nombre,apellido,correo,numero,estadoCivil,ciudad);
         return aux;
+
+
+    }
+
+    public void obtenerInformacion(){
+        //Como ejemplo se obtendra la primera tablet
+        String espe = obtenerEspecificaciones();
+        System.out.println(espe);
+
+    }
+
+    public String obtenerEspecificaciones() {
+        //Como ejemplo se obtendra la primera tablet
+        Tablets tSelelccionada = this.tablets.get(0);
+        System.out.println("Especificaciones: ");
+        return tSelelccionada.toString();
+
+
+    }
+
+    public void agregarPcEscritotio(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Marca: ");
+        String marca = sc.next();
+        System.out.println("MemoriaRam");
+        String memoriaRam = sc.next();
+        System.out.println("MemoriaAlmacenamiento: ");
+        String memoriaAlmacenamiento = sc.next();
+        System.out.println("Procesador: ");
+        String procesador = sc.next();
+        System.out.println("Modelo: ");
+        String modelo = sc.next();
+        System.out.println("Año: ");
+        String ano = sc.next();
+        System.out.println("Precio: ");
+        String precio = sc.next();
+        System.out.println("Stock");
+        String stock = sc.next();
+        System.out.println("Tarjeta de video: ");
+        String tarjeta = sc.next();
+        System.out.println("Fuente de poder: ");
+        String fuente = sc.next();
+        System.out.println("Chasis: ");
+        String cahsis = sc.next();
+        System.out.println("marca pantalla: ");
+        String maracapantalla = sc.next();
+        System.out.println("Modelo pantalla: ");
+        String modeloPantalla = sc.next();
+        System.out.println("Año: ");
+        String anoPantalla = sc.next();
+
+        Pantalla p = new Pantalla(maracapantalla,modeloPantalla,anoPantalla);
+
+        ComputadoresEscritorio c = crearComputadoresEscritorio(marca,memoriaRam,memoriaAlmacenamiento,procesador,modelo,ano,precio,stock,tarjeta,fuente,cahsis,p);
+        this.computadoresEscritorios.add(c);
+    }
+
+    private ComputadoresEscritorio crearComputadoresEscritorio(String marca, String memoriaRam, String memoriaAlmacenamiento, String procesador, String modelo, String ano, String precio, String stock, String tarjeta, String fuente, String cahsis, Pantalla p) {
+        ComputadoresEscritorio aux = new ComputadoresEscritorio(marca,memoriaRam,memoriaAlmacenamiento,procesador,modelo,ano,precio,stock,tarjeta,fuente,cahsis,p);
+        return aux;
+    }
+
+    public void agregarNotebook(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Marca: ");
+        String marca = sc.next();
+        System.out.println("MemoriaRam");
+        String memoriaRam = sc.next();
+        System.out.println("MemoriaAlmacenamiento: ");
+        String memoriaAlmacenamiento = sc.next();
+        System.out.println("Procesador: ");
+        String procesador = sc.next();
+        System.out.println("Modelo: ");
+        String modelo = sc.next();
+        System.out.println("Año: ");
+        String ano = sc.next();
+        System.out.println("Precio: ");
+        String precio = sc.next();
+        System.out.println("Stock");
+        String stock = sc.next();
+        System.out.println("resolucion: ");
+        String resolucion = sc.next();
+        System.out.println("Teclado: ");
+        String teclado = sc.next();
+        System.out.println("Bateria: ");
+        String bateria = sc.next();
+
+        this.notebooks.add(new Notebooks(marca,memoriaRam,memoriaAlmacenamiento,procesador,modelo,ano,precio,stock,resolucion,teclado,bateria));
+
+
+    }
+    public void agregarTablets(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Marca: ");
+        String marca = sc.next();
+        System.out.println("MemoriaRam");
+        String memoriaRam = sc.next();
+        System.out.println("MemoriaAlmacenamiento: ");
+        String memoriaAlmacenamiento = sc.next();
+        System.out.println("Procesador: ");
+        String procesador = sc.next();
+        System.out.println("Modelo: ");
+        String modelo = sc.next();
+        System.out.println("Año: ");
+        String ano = sc.next();
+        System.out.println("Precio: ");
+        String precio = sc.next();
+        System.out.println("Stock");
+        String stok = sc.next();
+        System.out.println("Resolucion: ");
+        String resolucion = sc.next();
+
+
+        ArrayList<String> accesorios = new ArrayList<>();
+        accesorios.add(sc.next());
+        accesorios.add(sc.next());
+        accesorios.add(sc.next());
+
+        this.tablets.add(new Tablets(marca,memoriaRam,memoriaAlmacenamiento,procesador,modelo,ano,precio,stok,resolucion,accesorios));
+
 
 
     }
--- src/test/java/testTienda.java
+++ src/test/java/testTienda.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 public class testTienda {
     @Test
     public void testAgregarCliente(){
@@ -11,6 +13,23 @@ public class testTienda {
         Cliente clienteObtenido = t.crearCliente("Jose","Matus","123","12345678",
                 "viudo","Temuco");
         Assertions.assertEquals(clienteEsperado.toString(),clienteObtenido.toString());
+
+    }
+    @Test
+    public void verificarDispositovo(){
+        ArrayList<String> ac = new ArrayList<>();
+        ac.add("lapiz");
+        ac.add("funda");
+        ArrayList <Tablets> tablets = new ArrayList<>();
+        Tablets t = new Tablets("Asus","8","512","i5","Nuevo","2010","5000","10","1080",ac);
+        tablets.add(t);
+        Tienda tienda = new Tienda(tablets);
+
+        String esperado = t.toString();
+        String recivido = tienda.obtenerEspecificaciones();
+
+        Assertions.assertEquals(esperado,recivido);
+
 
     }
 
